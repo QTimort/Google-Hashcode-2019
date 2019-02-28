b286f91def487ca35aae9755ca71476758d54e0c
Flictuum <florent.bertrand@epitech.eu>
2019-02-28 22:28:25 +0100

    add hor

--- hashcode2019/src/main/java/eu/epitech/hashcode_2019/solver/SolverImpl.java
+++ hashcode2019/src/main/java/eu/epitech/hashcode_2019/solver/SolverImpl.java
@@ -5,15 +5,23 @@ import eu.epitech.hashcode_2019.model.Slide;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 
 public class SolverImpl implements ISolver {
 
     private final List<Slide> mergedSlides = new ArrayList<>();
+    private final List<Slide> HorSlides = new ArrayList<>();
+    private final List<Slide> finalSlideshow = new ArrayList<>();
+
 
     public List<Slide> solve() {
         createMatchedSlideByPopularity();
-        return new ArrayList<>(mergedSlides);
+        createHorSlides();
+        ArrayList<Slide> tmp = new ArrayList<>(mergedSlides);
+        tmp.addAll(HorSlides);
+        createMatchedAllSlideByPopularity(tmp);
+        return new ArrayList<>(finalSlideshow);
     }
 
     private void createMatchedSlideByPopularity() {
@@ -29,6 +37,30 @@ public class SolverImpl implements ISolver {
             OpDictionnary.removeImageId(imageFirst);
             OpDictionnary.removeImageId(imageLast);
             mergedSlides.add(newSlide);
+        }
+    }
+
+    private void createHorSlides() {
+        for (final int imageId : OpDictionnary.horiImageIds) {
+            Slide newSlide = new Slide();
+            List<Integer> images = Arrays.asList(imageId);
+            newSlide.setImageIds(images);
+            OpDictionnary.removeImageId(imageId);
+            HorSlides.add(newSlide);
+        }
+    }
+
+    private void createMatchedAllSlideByPopularity(List<Slide> mergedSlidesWithHor) {
+        Deque<Slide> slides = OpDictionnary.queueMySlides(mergedSlidesWithHor);
+        while (!slides.isEmpty()) {
+            if (slides.size() < 2)
+                break;
+            Slide newSlide = new Slide();
+            Slide slideLast = slides.pollFirst();
+            Slide slideFirst = slides.pollLast();
+
+            finalSlideshow.add(slideFirst);
+            finalSlideshow.add(slideLast);
         }
     }
 }
